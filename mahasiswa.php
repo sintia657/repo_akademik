5c8d8fa7999e3a92827521e2d1a935e77d55797f
sintia657 <putrisintya308@gmail.com>
2025-03-10 11:47:07 +0700

    Create mahasiswa.php
    Buat modul mahasiswa

---
+++ mahasiswa.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Data Mahasiswa</title>
+</head>
+<body>
+<h6>Data Mahasiswa Amik AMP</h6>
+<hr>
+<h6>NIM : 23110004</h6><br>
+<h6>Nama : Sintia Putri Rama</h6><br>
+<a href="index.php">Kembali</a>
+</body>
+</html>
