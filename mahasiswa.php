75d14f62b82562d8849ee43fbd98679b44c0ffc1
sintia657 <putrisintya308@gmail.com>
2025-03-17 11:12:36 +0700

    Update mahasiswa.php
    penambahan nama kampus

--- mahasiswa.php
+++ mahasiswa.php
@@ -10,7 +10,8 @@
   <h6>Ketua Kelas</h6><br>
   <h6>Nim : 23110042</h6><br>
   <h6>Joko Brilian</h6><br>
-  <h6>Tahun 2025</h6>
+  <h6>Tahun 2025</h6><br>
+  <h6>Amik Mahaputra Riau</h6>
 <a href="index.php">Kembali</a>
 </body>
 </html>
